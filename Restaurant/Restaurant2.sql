aebebc90b2ee8c5164a42aa63fb2e62375673b10
JediMarty <130549056+JediMarty@users.noreply.github.com>
2025-02-18 11:48:51 +0200

    Add files via upload

--- Restaurant/Restaurant2.sql
+++ Restaurant/Restaurant2.sql
@@ -1,1 +1,60 @@
-SELECT pos_id, Pos_NAME FROM positions
+CREATE TABLE Rtables (
+    tID int,
+    tNAME varchar(3),
+    status varchar(10),
+    PRIMARY KEY (tID)
+);
+
+CREATE SEQUENCE POS_rtables START WITH 1 NOCACHE ORDER;
+
+create or replace TRIGGER Rtables_id_AUTO
+BEFORE INSERT ON Rtables  
+FOR EACH ROW  
+WHEN (NEW.tID IS NULL)  
+BEGIN 
+:NEW.tID := POS_rtables.NEXTVAL; 
+END; 
+
+create or replace PROCEDURE P_Rtables
+(tname Rtables.tNAME%TYPE,
+tstatus Rtables.status%TYPE)
+AS
+ BEGIN
+ INSERT INTO Rtables(tNAME,status)
+ Values (tname,tstatus);
+END;
+
+CREATE TABLE Orders (
+    ordersID int,
+    tID int,
+    FOREIGN KEY(tID) REFERENCES Rtables(tID),
+    ID int,
+    FOREIGN KEY(ID) REFERENCES EMPLOYEES(ID),
+    mID int,
+    FOREIGN KEY(mID) REFERENCES Menu_Items(mID),
+    timeordering DATE,
+    PRIMARY KEY (ordersID)
+);
+
+CREATE SEQUENCE POS_ORDERS START WITH 1 NOCACHE ORDER;
+
+create or replace TRIGGER Orders_id_AUTO
+BEFORE INSERT ON Orders  
+FOR EACH ROW  
+WHEN (NEW.ordersID IS NULL)  
+BEGIN 
+:NEW.ordersID := POS_ORDERS.NEXTVAL; 
+END; 
+
+create or replace PROCEDURE P_Orders
+(rtID Orders.tID%TYPE,
+eID Orders.ID%TYPE,
+menuID Orders.mID%TYPE,
+otime Orders.timeordering%TYPE)
+AS
+ BEGIN
+ INSERT INTO Orders(tID,ID,mID,timeordering)
+ Values (rtID,eID,menuID,otime);
+END;
+
+
