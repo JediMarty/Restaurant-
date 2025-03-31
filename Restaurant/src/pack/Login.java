7825adf313063e8ea8e8cef0053b5ac6a7cd750b
JediMarty <130549056+JediMarty@users.noreply.github.com>
2025-03-31 16:45:12 +0300

    Update Login.java

--- Restaurant/src/pack/Login.java
+++ Restaurant/src/pack/Login.java
@@ -2,30 +2,19 @@ package pack;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-// fix this public or something else ???
-public class Login {
+class Login {
 
-	String idbcURL = "jdbc:oracle:thin:@localhost:1521:XE";
-	String user = "marti";
-	String password = "marti";
-	
-	String resultHashedPass;
+	static String resultHashedPass;
 	
 	Login() {
-	try {
 		
 		MessageDigest digest;
 		
 		try {
 			digest = MessageDigest.getInstance("SHA-256"); //Hash algorithm
-		
-			byte[] hashedBytes = digest.digest(RestaurantMain.password.getText().getBytes());
+			String getPasswordText = new String(RestaurantMain.password.getPassword());
+			byte[] hashedBytes = digest.digest(getPasswordText.getBytes());
 			
 			StringBuilder hexString = new StringBuilder();
 			for (byte b: hashedBytes) {
@@ -39,37 +28,8 @@ public class Login {
 			e.printStackTrace();
 		}
 		
-		Connection conn = DriverManager.getConnection(idbcURL,user,password); //The connection
+		SQL_Handler.loginsql(resultHashedPass);
 		
-		String sql = "SELECT e.Firstname, p.Pos_NAME "
-				+ "FROM EMPLOYEES e "
-				+ "JOIN positions p ON e.pos_id = p.pos_id "
-				+ "WHERE Firstname = ? AND Password = ? ";
-		
-		PreparedStatement statement = conn.prepareStatement(sql);
-		statement.setString(1, RestaurantMain.username.getText());
-		statement.setString(2, resultHashedPass);
-		
-		ResultSet result = statement.executeQuery(); 
-		
-		if (result.next()) //Results crawling
-		{
-			System.out.print("Здрастиии");
-			
-			
-		}
-		
-		else { // The data sent is invalid!
-			System.out.print("Няма такъв потребител!");	
-		}
-		
-		conn.close();
-	
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
 	}
 
 }
